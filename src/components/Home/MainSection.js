02dfafa76323d89487442751afd40a97d85aa065
rashaabdulrazzak <rasha.abdulrazzak@gmail.com>
2022-02-16 10:43:34 +0300

    add image for main section

--- src/components/Home/MainSection.js
+++ src/components/Home/MainSection.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { Row, Col } from "react-bootstrap";
 import TitledText from "../../TitledText";
-import hands from "../../img/hands.jpg";
+// import hands from "../../img/hands.jpg";
+import hands from "../../img/main/20943414.jpg";
+
 function MainSection() {
   return (
     <Row className="home-cover d-flex align-items-center justify-content-center bg-gray font-link">
@@ -17,8 +19,17 @@ function MainSection() {
           text="Pay it forward you will need that one day"
         />
       </Col>
-      <Col xs={12} md={6} className="d-flex justify-content-center px-lg-5">
-        <img src={hands} alt="Let's support" style={{ height: "250px" }} />
+      <Col
+        xs={12}
+        md={6}
+        style={{ marginTop: "-30px;" }}
+        className="bg-image d-flex justify-content-center px-lg-5"
+      >
+        <img
+          src={hands}
+          alt="Let's support"
+          style={{ height: "400px", marginTop: "-30px;" }}
+        />
       </Col>
     </Row>
   );
